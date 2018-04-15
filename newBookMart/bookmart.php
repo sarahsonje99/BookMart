3765be3818fb6c43c0d4752442c1b7cc6d97fb1b
shlokas <shlokasapru@gmail.com>
2018-04-15 10:22:52 +0530

    book.php from DB

--- newBookMart/bookmart.php
+++ newBookMart/bookmart.php
@@ -1,45 +1,38 @@
 
 <?php
-session_start();
 $username = "root";
 $password = "";
 $database = "bookmart";
 $con = mysqli_connect("localhost",$username,$password,$database);
-if(!$con){
-    die("Connection failed: ".mysqli_connect_error());
-}
+
 
 if(isset($_POST['username'])){
     $uname = $_POST['username'];
     $pass = $_POST['password'];
     if($_POST['isSeller'] == 'true')
-        $sql = "SELECT * FROM seller WHERE username='".$uname."' AND password='".$pass."' limit 1";
-    else if ($_POST['isSeller'] == 'false')
-        $sql = "SELECT * FROM customer WHERE username='".$uname."' AND password='".$pass."' limit 1";
-    //$sql2 = "INSERT INTO seller(username, password, email, seller_fullname) VALUES('admins','admins','admins@gmail.com','admin seller');";
+        $sql = "SELECT * FROM seller WHERE uname='".$uname."' AND password='".$pass."' limit 1";
+    if($_POST['isSeller'] == 'false')
+        $sql = "SELECT * FROM customer WHERE uname='".$uname."' AND password='".$pass."' limit 1";
     $result = mysqli_query($con,$sql);
-    //$result1 = mysqli_query($con, $sql2);
     if(!empty($result)){
-        //echo "is a seller";}
         $row = mysqli_fetch_assoc($result);
         // if($_POST['isSeller'] == 'false')
-        //echo $row["username"];
-        $_SESSION["username"] = $row["username"];
-        if($_POST['isSeller'] == 'false')
-            $_SESSION["usertype"] = "customer";
-        else if($_POST["isSeller"] == 'true')  
-            $_SESSION["usertype"] = "seller";
         header("Location: home.php");
+        session_start();
+        $_SESSION["username"] = $row["name"];
+        if($_POST['isSeller'] == 'false')
+            $_SESSION["usertype"] = "cust";
+        else    
+            $_SESSION["usertype"] = "seller";
         exit();
     }
     else{
-        //$_SESSION["loggedIn1"] = "false";
         echo "Incorrect Login Details";
         exit();
     }
 
-}
 
+}
 
 ?>
 
@@ -189,12 +182,6 @@ if(isset($_POST['username'])){
                 return false;
             });
         });
-
-        function viewDet(){
-            var bookid = 12;
-            $.post('book.php',{bookid});
-            document.location.href='book.php';
-        }
         </script>
     </head>
     <body >    
@@ -211,7 +198,7 @@ if(isset($_POST['username'])){
                             <a class="navbar-brand" href="#">BookMart</a>
                         </div>
                         <ul class="nav navbar-nav" style="text-indent:0%">
-                            <li class=""><a href="home.php">Home</a></li>
+                            <li class=""><a href="home.html">Home</a></li>
                             <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Genre <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Thriller</a></li>
@@ -219,23 +206,8 @@ if(isset($_POST['username'])){
                                 <li><a href="#">Adventure</a></li>
                             </ul>
                             </li>
-                            <li>
-                            <?php if($_SESSION['usertype'] == "customer"): ?>
-                                <a href="orders.php">My Orders</a>
-                            <?php elseif($_SESSION['usertype'] == "seller"): ?>
-                                <a href="shelf.php">My Shelfs</a>
-                            <?php else: ?>
-                                <li ><a href="#ex1" rel="modal:open"> My Shelf/My Orders</a>
-                            <?php endif; ?>
-                            </li>
-                            <li>
-                            <?php if ($_SESSION['usertype']) : ?>
-                                <a href="profile.php"> Profile</a></li> 
-                            <?php else: ?>
-                                <a href="#ex1" rel="modal:open"> Profile</a>   
-                            <?php endif; ?> 
-
-                            
+                            <li><a href="#">My Orders/My Shelf</a></li>
+                            <li><a href="#">Profile</a></li>
                         </ul>
                         <form class="navbar-form navbar-left" action="">
                             <div class="form-group">
@@ -245,14 +217,8 @@ if(isset($_POST['username'])){
                             </button>
                           </form>
                         <ul class="nav navbar-nav navbar-right">
-                            <?php if ($_SESSION['usertype'] ): ?>
-                                <li><a > <?php echo "Hi, ". $_SESSION["username"]. "!"; ?></a></li>
-                                <li ><a> Logout</a>
-                                </li> 
-                            <?php else: ?>
-                                <li ><a href="#ex1" rel="modal:open"> Login</a>
-                                </li>   
-                            <?php endif; ?>                            
+                            <li ><a href="#ex1" rel="modal:open"> Login</a>
+                            </li>
                             <li ><a></a>
                             </li>
                         </ul>
@@ -264,7 +230,7 @@ if(isset($_POST['username'])){
         <div class="modal login_cont" id="ex1" style="display:none;">
 	            <div class="form">
                 <h1 style="margin-top:0px">Login</h1>
-                <form id="myForm" role="form" action="home.php" method="post" enctype="multipart/form-data">
+                <form id="myForm" role="form" action="#" method="post" enctype="multipart/form-data">
                     <p style="font-size:16px; text-align: left; margin-left:27px; margin-bottom:0px;"><br>Username </p>
                     <input class="namebox" type="text" id="email" name="username">
                     <p style="font-size:16px; text-align: left; margin-left:27px; margin-bottom:0px;"><br>Password </p>
@@ -292,7 +258,7 @@ if(isset($_POST['username'])){
                 <br>
                 <div class="row vertical-dist-between-tiles">
                 
-                    <div class="col-sm-2 tile" onClick="viewDet();" >
+                    <div class="col-sm-2 tile"  >
                         <div class="row image">
                             <span><img class="img-responsive" style="width:100%; height: 100%;" src = "https://resizing.flixster.com/7-QFEH63yycuAzN5jjo6fevs0qg=/206x305/v1.bTsxMTIwOTQ2MDtqOzE3NzI0OzEyMDA7MzMxODs0NDI0"></span>
                         </div>
